fb3d94eb7eb75a0386c0a4150514c96ddda2bf37
Luca Bracone <lucabr123@gmail.com>
2021-12-06 13:06:22 +0100

    day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,44 +1,58 @@
+type Num = u64;
 #[aoc(day6, part1)]
-fn first(input: &str) -> u64 {
+fn first(input: &str) -> Num {
     compute(80, input)
 }
 
 #[aoc(day6, part2)]
-fn second(input: &str) -> u64 {
+fn second(input: &str) -> Num {
     compute(256, input)
 }
 
-fn compute(days: usize, input: &str) -> u64 {
-    let mut fishes: Vec<u8> = input.split(',').map(|s| s.parse().unwrap()).collect();
+fn compute(days: usize, input: &str) -> Num {
+    let mut fishes: [Num; 9] =
+        input
+            .split(',')
+            .map(|n| n.parse().unwrap())
+            .fold([0; 9], |mut arr, n: usize| {
+                arr[n] += 1;
+                arr
+            });
+    
     for _ in 0..days {
-        let mut babbies = 0;
-        for fish in &mut fishes {
-            if *fish == 0 {
-                *fish = 6;
-                babbies += 1;
-            } else {
-                *fish -= 1;
-            }
-        }
-        for _ in 0..babbies {
-            fishes.push(8)
-        }
+        fishes[..=6].rotate_left(1);
+        let babbies = fishes[6];
+        fishes[6] += fishes[7];
+        fishes[7] = fishes[8];
+        fishes[8] = babbies;
     }
-    fishes.len() as u64
+    fishes.iter().sum()
 }
 
 #[cfg(test)]
 mod test {
+    const INPUT: &str = "3,4,3,1,2";
+
+    #[test]
+    fn mini() {
+        let input = "2";
+        assert_eq!(compute(5, input), 2);
+    }
+
     use super::*;
     #[test]
     fn one() {
-        let input = "3,4,3,1,2";
-        assert_eq!(first(input), 5934);
+        assert_eq!(first(INPUT), 5934);
     }
 
     #[test]
     fn two() {
-        let input = "3,4,3,1,2";
-        assert_eq!(second(input), 26984457539);
+        assert_eq!(second(INPUT), 26984457539);
+    }
+
+    #[test]
+    #[ignore]
+    fn bigboy_1() {
+        compute(9999999, INPUT);
     }
 }
