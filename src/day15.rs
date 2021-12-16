2ecf7210651f1a72b3354c7774d83d841abda0b4
Luca Bracone <lucabr123@gmail.com>
2021-12-16 20:53:31 +0100

    about to do one more major rewrite

--- src/day15.rs
+++ src/day15.rs
@@ -1,34 +1,6 @@
 use crate::matrix::Matrix;
 use std::collections::{HashMap, HashSet};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Direction {
-    Left,
-    Right,
-    Up,
-    Down,
-}
-
-impl Direction {
-    fn r#move(&self, x: usize, y: usize) -> (usize, usize) {
-        match self {
-            Self::Left => (x - 1, y),
-            Self::Right => (x + 1, y),
-            Self::Up => (x, y - 1),
-            Self::Down => (x, y + 1),
-        }
-    }
-
-    fn opp(&self) -> Self {
-        match self {
-            Self::Left => Self::Right,
-            Self::Right => Self::Left,
-            Self::Up => Self::Down,
-            Self::Down => Self::Up,
-        }
-    }
-}
-
 #[aoc_generator(day15)]
 fn gen(input: &str) -> Matrix<u32> {
     let items = input.chars().filter_map(|c| c.to_digit(10));
@@ -41,7 +13,11 @@ fn gen(input: &str) -> Matrix<u32> {
 #[aoc(day15, part1)]
 fn first(field: &Matrix<u32>) -> u64 {
     let mut memory = HashMap::new();
-    field.risk_from(0, 0, Direction::Up, &mut memory)
+    let res = field.risk_from(0, 0, Vec::new(), &mut memory);
+    let mut keys = memory.keys().collect::<Vec<_>>();
+    keys.sort_unstable();
+    println!("{:?}", field.final_path_from(0, 0, &memory, Vec::new()));
+    res
 }
 
 trait Risk {
@@ -49,15 +25,17 @@ trait Risk {
         &self,
         x: usize,
         y: usize,
-        came_from: Direction,
+        visited: Vec<(usize, usize)>,
         memory: &mut HashMap<(usize, usize), u64>,
     ) -> u64;
-}
 
-enum Edge {
-    Min,
-    Max,
-    Neither,
+    fn final_path_from(
+        &self,
+        x: usize,
+        y: usize,
+        memory: &HashMap<(usize, usize), u64>,
+        visited: Vec<(usize, usize)>,
+    ) -> Vec<((usize, usize), u32)>;
 }
 
 impl Risk for Matrix<u32> {
@@ -65,114 +43,94 @@ impl Risk for Matrix<u32> {
         &self,
         x: usize,
         y: usize,
-        came_from: Direction,
+        mut visited: Vec<(usize, usize)>,
         memory: &mut HashMap<(usize, usize), u64>,
     ) -> u64 {
+        visited.push((x, y));
 
-        println!("x: {}, y: {}, from: {:?}, cur: {}", x, y, came_from, self[(x,y)]);
-        println!("memory: {:?}", memory);
-
-        let y_edge = if y == 0 {
-            Edge::Min
-        } else if y == self.height() - 1 {
-            Edge::Max
-        } else {
-            Edge::Neither
-        };
-        let x_edge = if x == 0 {
-            Edge::Min
-        } else if x == self.width() - 1 {
-            Edge::Max
-        } else {
-            Edge::Neither
-        };
-
-        if let Some(val) = memory.get(&(x, y)) {
-            *val
-        } else {
-            let val = match (x_edge, y_edge) {
-                // Corners
-                (Edge::Max, Edge::Max) => 0,
-                (Edge::Min, Edge::Max) => self.risk_from(x + 1, y, Direction::Left, memory),
-                (Edge::Max, Edge::Min) => self.risk_from(x, y + 1, Direction::Up, memory),
-                (Edge::Min, Edge::Min) => *[
-                    self.risk_from(x + 1, y, Direction::Left, memory),
-                    self.risk_from(x, y + 1, Direction::Up, memory),
-                ]
-                .iter()
-                .min()
-                .unwrap(),
-                // Edges
-                (Edge::Neither, Edge::Min) => {
-                    if came_from == Direction::Down {
-                        self.risk_from(x + 1, y, Direction::Left, memory)
-                    } else {
-                        *[
-                            self.risk_from(x + 1, y, Direction::Left, memory),
-                            self.risk_from(x, y + 1, Direction::Up, memory),
-                        ]
-                        .iter()
-                        .min()
-                        .unwrap()
-                    }
-                }
-                (Edge::Neither, Edge::Max) => {
-                    if came_from == Direction::Up {
-                        self.risk_from(x + 1, y, Direction::Left, memory)
-                    } else {
-                        *[
-                            self.risk_from(x + 1, y, Direction::Left, memory),
-                            self.risk_from(x, y - 1, Direction::Down, memory),
-                        ]
-                        .iter()
-                        .min()
-                        .unwrap()
-                    }
-                }
-                (Edge::Min, Edge::Neither) => {
-                    if came_from == Direction::Right {
-                        self.risk_from(x, y + 1, Direction::Up, memory)
-                    } else {
-                        *[
-                            self.risk_from(x + 1, y, Direction::Left, memory),
-                            self.risk_from(x, y + 1, Direction::Up, memory),
-                        ]
-                        .iter()
-                        .min()
-                        .unwrap()
-                    }
-                }
-                (Edge::Max, Edge::Neither) => {
-                    if came_from == Direction::Left {
-                        self.risk_from(x, y + 1, Direction::Up, memory)
-                    } else {
-                        *[
-                            self.risk_from(x, y + 1, Direction::Up, memory),
-                            self.risk_from(x - 1, y, Direction::Right, memory),
-                        ]
-                        .iter()
-                        .min()
-                        .unwrap()
-                    }
-                }
-                (Edge::Neither, Edge::Neither) => [
-                    Direction::Right,
-                    Direction::Down,
-                    Direction::Up,
-                    Direction::Left,
-                ]
-                .iter()
-                .filter(|&&dir| dir != came_from)
-                .map(|dir| {
-                    let (x_mv, y_mv) = dir.r#move(x, y);
-                    self.risk_from(x_mv, y_mv, dir.opp(), memory)
-                })
-                .min()
-                .unwrap(),
-            };
-            memory.insert((x, y), self[(x, y)] as u64 + val);
-            memory[&(x, y)]
+        // If we start from the destination, the risk is just the value inside
+        if x == self.width() - 1 && y == self.height() - 1 {
+            memory.insert((x, y), self[(x, y)] as u64);
+            self[(x, y)] as u64
         }
+        // Otherwise if we already know the risk from a given point, just return that
+        else if let Some(&val) = memory.get(&(x, y)) {
+            val
+        // Otherwise find the guys you can visit and recur
+        } else {
+            let mut can_go = self.rook_neighbor_indices(x, y).filter(|&(xn, yn)| {
+                // If xn is close to either edge, can't go up
+                if (xn < 2 || xn >= self.width() - 2) && yn + 1 == y {
+                    return false;
+                }
+                // If yn is close to either edge, can't go left
+                if (yn < 2 || yn >= self.height() - 2) && xn + 1 == x {
+                    return false;
+                }
+                // If the new position is in contact with one of the nodes we already visited,
+                // we can't go there
+                if visited.iter().any(|&(xo, yo)| {
+                    if xo == x && yo == y {
+                        false
+                    } else {
+                        (xn as isize - xo as isize).abs() + (yn as isize - yo as isize).abs() <= 1
+                    }
+                }) {
+                    return false;
+                }
+                // Finally can't go to a node we already visited
+                !visited.contains(&(xn, yn))
+            });
+
+            if x == 8 && y == 6 {
+                println!("visited: {:?}", visited);
+            }
+
+            let val = if visited.len() == 1 {
+                can_go
+                    .map(|(xn, yn)| self.risk_from(xn, yn, visited.clone(), memory))
+                    .min()
+                    .unwrap()
+            } else {
+                self[(x, y)] as u64
+                    + can_go
+                        .map(|(xn, yn)| {
+                            if xn == 8 && yn == 5 {
+                                println!("({},{}), can_go: {:?}", x, y, (xn, yn));
+                            }
+
+                            (xn, yn, self.risk_from(xn, yn, visited.clone(), memory))
+                        })
+                        .inspect(|(xn, yn, elem)| println!("({}, {}) - found val {} with ({},{})", x, y, elem, xn, yn))
+                        .map(|(_,_, val)| val)
+                        .min()
+                        .unwrap()
+            };
+            memory.insert((x, y), val);
+
+            val
+        }
+    }
+
+    fn final_path_from(
+        &self,
+        x: usize,
+        y: usize,
+        memory: &HashMap<(usize, usize), u64>,
+        mut visited: Vec<(usize, usize)>,
+    ) -> Vec<((usize, usize), u32)> {
+        visited.push((x, y));
+        let (xb, yb) = self
+            .rook_neighbor_indices(x, y)
+            .filter(|neighbor| !visited.contains(neighbor))
+            .min_by_key(|neighbor| memory[neighbor])
+            .unwrap();
+        if x == self.width() - 1 && y == self.height() - 1 {
+            return vec![((x, y), self[(x, y)])];
+        }
+        let mut res = self.final_path_from(xb, yb, memory, visited.clone());
+        res.push(((x, y), self[(x, y)]));
+        res
     }
 }
 
@@ -202,7 +160,7 @@ mod test {
     fn mini() {
         let input = gen("11
                          91");
-        assert_eq!(first(&input), 3);
+        assert_eq!(first(&input), 2);
     }
 
     #[test]
@@ -213,6 +171,6 @@ mod test {
                          191111
                          199999
                          111111");
-        assert_eq!(first(&input), 3);
+        assert_eq!(first(&input), 18);
     }
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -33,21 +33,21 @@ impl<T> Matrix<T> {
         self.vec.get(x + y * self.width)
     }
 
-    #[allow(dead_code)]
-    pub fn neighbors_of(&self, x: usize, y: usize) -> [Option<&T>; 8] {
-        let x: isize = x.try_into().unwrap();
-        let y: isize = y.try_into().unwrap();
-
-        let top = self.get(x, y + 1);
-        let topright = self.get(x + 1, y + 1);
-        let right = self.get(x + 1, y);
-        let botright = self.get(x + 1, y - 1);
-        let bot = self.get(x, y - 1);
-        let botleft = self.get(x - 1, y - 1);
-        let left = self.get(x - 1, y);
-        let topleft = self.get(x - 1, y + 1);
-
-        [top, topright, right, botright, bot, botleft, left, topleft]
+    pub fn rook_neighbor_indices(&self, x: usize, y: usize) -> impl Iterator<Item = (usize, usize)> {
+        let mut res = Vec::new();
+        if y > 0 {
+            res.push((x,y - 1));
+        }
+        if x > 0 {
+            res.push((x - 1, y));
+        }
+        if y < self.height - 1 {
+            res.push((x, y + 1));
+        }
+        if x < self.width - 1 {
+            res.push((x + 1, y));
+        }
+        res.into_iter()
     }
 
     pub fn neighbor_indices(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
@@ -77,19 +77,12 @@ impl<T> Matrix<T> {
         res
     }
 
-    pub fn row(&self, i: usize) -> Vec<T>
-    where
-        T: Clone,
-    {
-        (0..self.width).map(|j| self[(i, j)].clone()).collect()
+    pub fn width(&self) -> usize {
+        self.width
     }
 
     pub fn height(&self) -> usize {
         self.height
-    }
-
-    pub fn width(&self) -> usize {
-        self.width
     }
 }
 
